18421676441eeae095cc8fa08153b0a27713dc96
mohamedsaiedd <mohamed00sd6@gmail.com>
2023-07-08 22:45:34 +0300

    add tailwindcss & hero

--- buddy/src/App.js
+++ buddy/src/App.js
@@ -1,37 +1,19 @@
 import './App.css';
 import { useState } from 'react';
-import axios from 'axios';
+import Hero from './components/Hero';
+
 
 function App() {
-  const [prompt, setPrompt] = useState("");
-  const [response, setResponse] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    axios.post('http://localhost:8000/buddy', { prompt })
-    .then((res) => {
-        console.log(res);
-        setResponse(res.data)
-      }).catch((err) => {
-        console.log(err);
-      })
-  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        input you text:
-        <input
-          type="text"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)} />
-      </label>
-      <input type="submit" value="Submit" />
-      <p>
-        the response is {response}
-      </p>
-    </form>
+    <>
+      <div>
+        <Hero />
+      </div>
+    </>
+
+
   );
 }
 
